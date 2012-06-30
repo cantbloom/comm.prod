7c5a5b4e20560c1707d17532e73d884c834b333b
Joshua Blum <joshblum@mit.edu>
2012-06-30 01:28:55 -0700

    added login and logout pages. styled reg form. fixed max's errors there too.

--- commProd/forms.py
+++ commProd/forms.py
@@ -1,21 +1,18 @@
 from django import forms
 from django.core import validators
 from commProd.models import UserProfile
-#from django.contrib.auth.models import User
  
 
 class RegForm(forms.Form):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'First Name'}), label="")
     last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'Last Name'}), label="")
-    shirt_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'Shirt Names'}), label="")
+    shirt_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'Comma, eparated, shirt names'}), label="")
     alt_email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder' : 'Alternative Email'}), label="",
         required=False)
     password = forms.CharField( widget=forms.PasswordInput(attrs={'placeholder' : 'Password'}), label="" )
     password_confirm = forms.CharField( widget=forms.PasswordInput(attrs={'placeholder' : 'Confirm Password'}), label="")
 
-    #first_name.widget.attrs.update({'placeholder' : 'First Name'})
-
-    def clean_password2(self):
+    def clean_password(self):
         password = self.cleaned_data.get('password')
         password_confirm = self.cleaned_data.get('password_confirm')
 
@@ -27,12 +24,6 @@ class RegForm(forms.Form):
 
     def clean_alt_email(self):
         data = self.cleaned_data['alt_email']
-        if UserProfile.objects.filter(alt_email=data).exists():
+        if UserProfile.objects.filter(alt_email=data).exists() or User.objects.filter(email=data).exists():
             raise forms.ValidationError('Email already in use.')
         return data
-    
-    # def save(self, new_data):
-    #   u = UserProfile.objects.get(e)
-    #   u.is_active = False
-    #   u.save()
-    #   return u
