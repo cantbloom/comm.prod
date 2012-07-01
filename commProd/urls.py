a33aa7723d23add9f72e781c3dc0c740e2a5ce5f
kmax12 <kmax12@gmail.com>
2012-07-01 02:09:50 -0700

    search results pages and routes

--- commProd/urls.py
+++ commProd/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('commProd.views',
     url(r'^users/(?P<user_id>\d+)/$', 'profile'),
     url(r'^users/(?P<username>\w+)/$', 'profile'),
     url(r'^home$', 'home'),
-    url(r'^popular$', 'search', {'title':'Popular comm.prods', 'orderBy': 'avg_score'}),
+    url(r'^popular$', 'search', {'title':'Popular comm.prods', 'orderBy': 'avg_score', 'direction': 'lh'}),
+    url(r'^recent$', 'search', {'title':'Recent comm.prods', 'orderBy': 'date', 'direction':'lh'}),
     url(r'^$', 'home'),
 )
