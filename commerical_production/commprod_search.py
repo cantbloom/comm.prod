dcfc01382aa100bc421b41ad312081a8bfb03d62
kmax12 <kmax12@gmail.com>
2012-06-30 21:27:24 -0700

    search hookedup kindof according jblum18

--- commerical_production/commprod_search.py
+++ commerical_production/commprod_search.py
@@ -1,25 +1,29 @@
 from commProd.models import CommProd, Rating, UserProfile
 
+#to test from commerical_production.commprod_search import commprod_search
 def commprod_search(cp_id=None, query=None, orderBy='date', direction='hl', username=None, startDate=None, endDate=None ):
-		commprods = CommProd.objects.all()
+	commprods = CommProd.objects.all()
 
-		if cp_id:
-			commprods.filter(id=cp_id)
+	if cp_id:
+		commprods = commprods.filter(id=cp_id)
 
-		if query:
-			commprods.filter(content__contains=query)
+	if username:
+		commprods = commprods.filter(user__username=username)
 
-		if orderBy:
-			#django defaults to high to low, so just andle low to high option 
-			if direction == 'lh':
-				orderBy = '-' + orderBy
-			commprods.orderBy(orderBy)
+	if query:
+		commprods = commprods.filter(content__contains=query)
 
-		if username:
-			commprods.filter(username=username)
+	if orderBy:
+		#django defaults to high to low, so just andle low to high option 
+		if direction == 'lh':
+			orderBy = '-' + orderBy
+		commprods = commprods.order_by(orderBy)
 
-		if startDate:
-			commprods.objects.filter(date__gte=startDate)
+	if startDate:
+		commprods = commprods.objects.filter(date__gte=startDate)
 
-		if endDate:
-			commprods.objects.filter(date__lte=endDate)
+	if endDate:
+		commprods = commprods.objects.filter(date__lte=endDate)
+
+	return list(commprods)
+
