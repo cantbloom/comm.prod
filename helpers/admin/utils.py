6cb69eb1604194260384a04755ba306784ef46ce
Joshua Blum <joshblum@mit.edu>
2012-07-01 00:04:58 -0700

    changed namespace. so many errors going to come

--- helpers/admin/utils.py
+++ helpers/admin/utils.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-import os, sha, random
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
+
+from email_templates import registration
+
+import os, re, random
+
 
 """
 ssh to athena and:
@@ -41,6 +47,10 @@ def add_users(filePath):
         print "Tried path:", os.getcwd() + "/" +filePath
         print "What is the actual path?"
 
+
+"""
+Create a new user with the given parameters
+"""
 def createUser(username, email, send_mail=False):
     try:
         user, created = User.objects.get_or_create(username=username, email=email)
@@ -54,3 +64,50 @@ def createUser(username, email, send_mail=False):
         return user, created
     except IntegrityError:
         return None
+
+
+"""
+Email current users
+"""
+def emailUsers(subject, html_content, user_emails):
+    text_content = strip_tags(html_content)
+    from_email = 'kanter@mit.edu'
+    msg = EmailMultiAlternatives(subject, text_content, from_email, user_emails)
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
+
+
+
+"""
+Send an email to inactive users who 
+have permission to receive mail to register
+"""
+def emailInactive():
+    users = User.objects.filter(is_staff = True)
+    for user in users:
+        content = registration['content'] % (user.username, 'http://localhost:8000/register/'+user.profile.activation_key + '/')
+        subject = registration['subject']
+        emails = [user.email]
+        print content, subject, emails
+        emailUsers(subject, content, emails)
+
+    return 'done'
+
+"""
+Test your regexs
+"""
+def testRegex():
+    while True:
+        cmd = raw_input("Type EXIT to quit, press enter to continue: ")
+        if cmd == "EXIT":
+            break
+        pattern =  re.compile(raw_input("Enter your regex: "))
+        query = raw_input("Enter input string to search: ")
+        if pattern.search(query) == None:
+            print "No match found"
+        
+        else:
+
+            for m in pattern.finditer(query):
+                print "\nI found the text '%s' starting at index '%d' and ending at index '%d'." % (m.group(), m.start(), m.end())
+
