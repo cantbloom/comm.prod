ba823fd3ee0d00927babeb464d777c0053f78958
Joshua Blum <joshblum@mit.edu>
2012-07-01 00:04:58 -0700

    changed namespace. so many errors going to come

--- commProd/models.py
+++ commProd/models.py
@@ -1,11 +1,46 @@
 from django.db import models
 from django.utils import simplejson as json
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 from django.db.models import Avg
-
 from datetime import date
-# Create your models here.
+
+ 
+class UserProfile(models.Model):  
+    user = models.OneToOneField(User)
+    #other fields here
+
+    activation_key = models.CharField(max_length=40, default='')
+    alt_email = models.EmailField(default='')
+    send_mail = models.BooleanField(default=False)
+    avg_score = models.FloatField()
+
+    def update_avg(self):
+        self.avg_score = Rating.objects.get(user=self.user).aggregate(Avg('score'))
+        self.save()
+
+    """
+    Takes an email, updates commprod objects 
+    associatd with the alt_email user to self. 
+    Also updates Rating objects to self
+    """
+    def mergeAndDelete(self, email):
+        to_delete = User.objects.filter(user__email=email)
+        if to_delete.exists():
+            CommProd.objects.filter(user=to_delete).update(user=self)
+            Rating.objects.filter(user=to_delete).update(user=self)
+            to_delete.delete()
+
+    def __str__(self):  
+          return "%s's profile" % self.user  
+
+User.profile = property(lambda u: u.get_profile())
+
+class ShirtName(models.Model):
+    user = models.ForeignKey(User)
+
+    number = models.CharField(max_length=40, default='')
+    name = models.CharField(max_length=40, default='Human Jizz Rag')
+    year = models.IntegerField()
 
 class CommProd(models.Model):
 	user = models.ForeignKey(User)
@@ -31,49 +66,5 @@ class Rating(models.Model):
 
 	def __unicode__(self):
 		return "%s voted a %s on commprod_id %s on %s " % (self.user.username, self.score, self.commprod.id, self.date)
- 
-class UserProfile(models.Model):  
-    user = models.OneToOneField(User)
-    #other fields here
-
-    activation_key = models.CharField(max_length=40, default='')
-    alt_email = models.EmailField(default='')
-    send_mail = models.BooleanField(default=False)
-    
-    """
-    Takes an email, updates commprod objects 
-    associatd with the alt_email user to self. 
-    Also updates Rating objects to self
-    """
-    def mergeAndDelete(self, email):
-        to_delete = User.objects.filter(user__email=email)
-        if to_delete.exists():
-            CommProd.objects.filter(user=to_delete).update(user=self)
-            Rating.objects.filter(user=to_delete).update(user=self)
-            to_delete.delete()
-
-    def __str__(self):  
-          return "%s's profile" % self.user  
-
-class ShirtName(models.Model):
-	user = models.ForeignKey(User)
-
-	number = models.CharField(max_length=40, default='')
-	name = models.CharField(max_length=40, default='Human Jizz Rag')
-	year = models.IntegerField()
 
 
-
-def create_user_profile(sender, instance, created, **kwargs):  
-    if created:  
-       profile, created = UserProfile.objects.get_or_create(user=instance)  
-
-post_save.connect(create_user_profile, sender=User) 
-
-def update_commprod_avg (sender, instance, **kwargs):
-	instance.commprod.update_avg()
-
-
-#post_save.connect(update_commprod_avg, sender=Rating)
-
-User.profile = property(lambda u: u.get_profile() )
--- commProd/views.py
+++ commProd/views.py
@@ -12,13 +12,10 @@ from django.contrib.auth import authenticate, login
 from commProd.models import CommProd, Rating, UserProfile, ShirtName
 from commProd.forms import RegForm
 from commerical_production import config
-from commerical_production.commprod_search import commprod_search
-from commerical_production.create_users import createUser
 
-import re
-import datetime
-import random
-
+from helpers.view_helpers import getRandomUsername, renderErrorMessage
+from helpers.commprod_search import commprod_search
+from helpers.admin.utils import createUser
 
 """
 Registration page. Visitor arrives wih activation key
@@ -204,36 +201,6 @@ def processProd(request):
             resp = "Success!"
     return HttpResponse(resp, mimetype="text/plain")
 
-###########HELPERS#############
 
-"""
-Returns a username to be rendered choosing randomly between
-first + last, username, and a shirt first_name.
-"""
-def getRandomUsername(user):
-    potentials = list(ShirtName.objects.filter(user=user))
-    potentials.append(user.first_name + user.last_name)
-    potentials.append(user.username)
-    first_last = user.first_name + " " +user.last_name
-    if (first_last.strip() != ""):
-        potentials.append(first_last)
-    return random.choice(potentials)
 
-""" 
-Give helpful messages for the retards.
-Returns a hero_err_template with the given data.
-Return this function to give user back an error page.
-"""
-def renderErrorMessage(request, page_title, hero_title):
-    if request.user.is_authenticated:
-        prof_href = "user/" + request.user.username
-    else:
-        prof_href = "/"
-    template_values = {
-        'page_title': page_title,
-        'user_profile' : prof_href,
-        'hero_err_title' : hero_title,
-    }
-    return render_to_response('hero_err_template.html', 
-        template_values, context_instance=RequestContext(request)) 
 
