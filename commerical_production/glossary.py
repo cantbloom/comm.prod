31d3e6b682fd25540fcb61ee112d9145e995226a
Joshua Blum <joshblum@mit.edu>
2012-07-04 02:49:10 -0700

    cron script

--- commerical_production/glossary.py
+++ commerical_production/glossary.py
@@ -1,4 +1,7 @@
 glossary = {
+    'public_enemy_#1' : "louis sobel",
 	'patlsotw' : 'protect against the louis sobels of the world',
-	'fls' : 'fuckin\' louis sobels'
+	'fls' : 'fuckin\' louis sobels',
+    'wldkchu' : "what louis doesn't know can't hurt us",
+    
 }  
