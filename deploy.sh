faed14ea8aa67a66cd75b2045b245bfd1e64ce94
Joshua Blum <joshblum@mit.edu>
2012-07-23 22:57:10 -0700

    "deploy"

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
-echo -n "Enter a commit message:"
+echo Enter a commit message:
 read -e COMMIT
 git add .
 git commit -am $COMMIT
-echo "Starting..."
+echo Starting...
 git pull staging staging
 git push staging staging
 git push staging staging:master
 
-echo -n "Push production?(y/n)"
+echo Push production?(y/n)
 read -e MASTER
-if [MASTER == 'y']
+if [$MASTER == 'y']
     then
         git checkout master
         git fetch staging
