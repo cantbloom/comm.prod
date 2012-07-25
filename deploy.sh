7e04005e50d6b3c3d795cc769473723cd7ade0a8
Joshua Blum <joshblum@mit.edu>
2012-07-25 00:41:11 -0700

    $COMMIT_MSG

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,33 @@
 #! /bin/bash
 
 
-# echo Enter a commit message:
-# read -e COMMIT
-# git add .
-# git commit -m $COMMIT
-# echo Starting...
-# git pull staging staging
-# git push staging staging
-# git push staging staging:master
-
-
-echo "Push production?(y/n)"
-read -e MASTER
-
-if ["$MASTER" == "y"]
-
-    then
-        git checkout master
-        git fetch staging
-        git reset --hard staging/master
-        git push origin master
+echo "Commit?(y/n) \c"
+read COMMIT
+if [ $COMMIT == 'y' ] ; then
+    echo "Enter a commit message: \c "
+    read COMMIT_MSG
+    git add .
+    git commit -m '$COMMIT_MSG'
 fi
 
-echo "...done"
+echo "Starting..."
+echo "Pulling staging staging"
+git pull staging staging
+echo "Pushing staging staging"
+git push staging staging
+echo "Pushing staging to staging-master"
+git push staging staging:master
+
+
+echo "Push production?(y/n) \c"
+read  MASTER
+
+if [ $MASTER == "y" ] ; then
+    git checkout master
+    git fetch staging
+    git reset --hard staging/master
+    echo "Pusing to origin master"
+    git push origin master
+fi
+
+echo "Done!"
