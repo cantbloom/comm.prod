bb3e88f3d837991766c739bfaf0754278b388922
Joshua Blum <joshblum@mit.edu>
2012-07-25 00:49:42 -0700

    sdf

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,32 @@
 
 echo "Commit?(y/n) \c"
 read COMMIT
-if [ $COMMIT == 'y' ] ; then
-    echo "Enter a commit message: \c "
-    read COMMIT_MSG
-    git add .
-    git commit -am '$COMMIT_MSG'
-fi
+`git commit -am $COMMIT`
+# if [ $COMMIT == 'y' ] ; then
+#     echo "Enter a commit message: \c "
+#     read COMMIT_MSG
+#     git add .
+#     git commit -am '$COMMIT_MSG'
+# fi
 
-echo "Starting..."
-echo "Pulling staging staging"
-git pull staging staging
-echo "Pushing staging staging"
-git push staging staging
-echo "Pushing staging to staging-master"
-git push staging staging:master
+# echo "Starting..."
+# echo "Pulling staging staging"
+# git pull staging staging
+# echo "Pushing staging staging"
+# git push staging staging
+# echo "Pushing staging to staging-master"
+# git push staging staging:master
 
 
-echo "Push production?(y/n) \c"
-read  MASTER
+# echo "Push production?(y/n) \c"
+# read  MASTER
 
-if [ $MASTER == "y" ] ; then
-    git checkout master
-    git fetch staging
-    git reset --hard staging/master
-    echo "Pusing to origin master"
-    git push origin master
-fi
+# if [ $MASTER == "y" ] ; then
+#     git checkout master
+#     git fetch staging
+#     git reset --hard staging/master
+#     echo "Pusing to origin master"
+#     git push origin master
+# fi
 
-echo "Done!"
+# echo "Done!"
