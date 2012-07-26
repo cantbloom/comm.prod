fe913f634f0523d9fec5c50c5f9f4218a43096d1
Joshua Blum <joshblum@mit.edu>
2012-07-26 02:18:36 -0700

    added grading modifiers

--- helpers/query_managers.py
+++ helpers/query_managers.py
@@ -207,15 +207,28 @@ def get_grade(user_score, std, mean):
     d = c - std
     f = d - std
     if user_score >= a:
-        return "A"
+        return "A" + grade_mod(user_score, a+std, a)
     elif user_score < a and user_score >= b:
-        return "B"
+        return "B" + grade_mod(user_score, a, b)
     elif user_score < b and user_score >= c:
-        return "C"
+        return "C" + grade_mod(user_score, b, c)
     elif user_score < c and user_score >= d:
-        return "D"
+        return "D" + grade_mod(user_score, c, d)
     else:
-        return "F"
+        return "F" + grade_mod(user_score, d, d-std)
+
+"""
+Returns +/- grades for user.
+Divides the upper and lower limit into three sections, return grade +  for upper, grade for middle, and grade - for lower. 
+"""
+def grade_mod(user_score, upper_limit, lower_limit):
+    sect = (upper_limit - lower_limit)/3.0 #partition to three sections
+    if user_score >= upper_limit-sect:
+        return "+"
+    elif user_score <= lower_limit+sect:
+        return "-"
+    return ""
+
 
 """
 Helper for trend data manager.
