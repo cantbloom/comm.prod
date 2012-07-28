d8f3b542f6f3f446b0587a35fec344f47fb28e10
Joshua Blum <joshblum@mit.edu>
2012-07-28 13:28:18 -0700

    updated daily cron

--- cron/daily_cron.sh
+++ cron/daily_cron.sh
@@ -3,19 +3,13 @@
 cd /home/cantbloom/commprod 
 source venv/bin/activate
 
-#update staging
-git checkout staging
-python manage.py update_trends
-python manage.py update_user_list
-git commit -am "update to user_list"
-git pull staging master
-git push staging master
-
-
-#update production
+#update production and staging
 git checkout master
 python manage.py update_trends
 python manage.py update_user_list
 git commit -am "update to user_list"
-git pull origin master
-git push origin master
+
+git pull staging master
+git push staging master
+
+git push origin master -- force
