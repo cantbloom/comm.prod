d452452b74748c488a79ba287e5eda2ffc697a93
cantbloom <cantbloom@mit.edu>
2012-08-01 16:12:04 -0400

    removed /bombers

--- commerical_production/urls.py
+++ commerical_production/urls.py
@@ -1,6 +1,29 @@
 from django.conf.urls import patterns, include, url
+from django.shortcuts import HttpResponse
+from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^bombers/', include('commerical_production.base_urls')),
-    url(r'', include('commerical_production.base_urls'))
+    url(r'^admin', include(admin.site.urls)),
+	url(r'^public/(?P<path>.*)$', 'django.views.static.serve',
+	 {'document_root': settings.MEDIA_ROOT}),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}),
+    url(r'^commprod/', include('commProd.urls')),
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
+)
+
+urlpatterns += patterns('commerical_production.views',
+    url(r'^register/(?P<key>\w+)', 'register'),
+    url(r'^confirm_email/(?P<key>\w+)', 'confirm_email'),
+    url(r'^claim_email$', 'claim_email'),
+    url(r'^users/(?P<username>.+)$', 'profile'),
+    url(r'^edit$', 'edit_profile'),
+    url(r'^welcome$', 'welcome'),
+    url(r'^home$', 'home'),
+    url(r'^feedback$', 'feedback'),
+    url(r'^login$', 'login', {'template_name': 'login.html'}),
+    url(r'^reset_password/(?P<key>\w+)', 'reset_password_confirm'),
+    url(r'^reset_password', 'reset_password'),
+    url(r'^$', 'home'),
 )
