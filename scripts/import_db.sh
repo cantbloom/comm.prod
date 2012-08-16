db8c05f3810cae313241bd32f692944cfc24e13f
Joshua Blum <joshblum@mit.edu>
2012-08-15 20:27:39 -0700

    added blog link

--- scripts/import_db.sh
+++ scripts/import_db.sh
@@ -9,10 +9,11 @@ echo "Exporting data..."
 mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_NAME > dumpfile.sql
 
 echo "Dropping old data"
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e `DROP DATABASE $MYSQL_NAME_DEV; CREATE DATABASE $MYSQL_NAME_DEV;`
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e DROP DATABASE $MYSQL_NAME_DEV; 
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e CREATE DATABASE $MYSQL_NAME_DEV;
 
 echo "Importing data..."
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_NAME_DEV < dumpfile.sql
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_NAME_DEV < dumpfile.sql;
 
 echo "Cleaning up..."
 rm dumpfile.sql
