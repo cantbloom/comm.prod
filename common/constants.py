993acb8f34f29def083755483695972c0c6b10ba
Joshua Blum <joshblum@mit.edu>
2012-08-20 00:31:20 -0400

    fixed parsing url regex

--- common/constants.py
+++ common/constants.py
@@ -1,12 +1,4 @@
 REGEX = {
-    'url_regex' : '''(?P<url>(?x)( # verbose identify URLs within text
-         (http|ftp|gopher) # make sure we find a resource type
-                       :// # ...needs to be followed by colon-slash-slash
-            (\w+[:.]?){2,} # at least two domain groups, e.g. (gnosis.)(cx)
-                      (/?| # could be just the domain name (maybe w/ slash)
-                [^ \n\r"]+ # or stuff then space, newline, tab, quote
-                    [\w/]) # resource name ends in alphanumeric or slash
-         (?=[\s\.,>)'"\]]) # assert: followed by white or clause ending
-                         )) # end of match group
-                         '''
+    'url_regex' : '(?P<url>(?:(?:https?|ftp|file)://|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$]))'
+
 }
