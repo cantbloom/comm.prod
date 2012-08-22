55906a6cebcd826b1cf07482cb4dccec2d8ccbb0
Joshua Blum <joshblum@mit.edu>
2012-08-22 11:51:18 -0400

    unicode def

--- donations/models.py
+++ donations/models.py
@@ -8,3 +8,6 @@ class Donation(models.Model):
     date = models.DateTimeField(auto_now=True)
     amount = models.FloatField(default=0.0)
     reason = models.TextField()
+
+    def __unicode__(self):
+        return 'Donation of $%s by %s on %s for %s' % (self.amount, self.user_profile.user.username, str(self.date, self.reason))
