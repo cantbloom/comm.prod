93196955025e6192df9ee3af87100d63af180bf2
Joshua Blum <joshblum@mit.edu>
2012-11-28 19:39:01 -0500

    fixed daily update cron

--- cron/daily_cron.sh
+++ cron/daily_cron.sh
@@ -3,18 +3,20 @@
 cd /home/commprod/commprod 
 source venv/bin/activate
 
-#update production 
-git checkout master
-#python manage.py update_user_list
 python manage.py update_trend_data
+
+#update production 
+#git checkout master
+#python manage.py update_user_list
 #git commit -am "dailycron: update to user_list"
 
 #git pull heroku master
 #git push heroku master
 #git push git master
 
+DATE=$(date +"%m-%d-%Y")
 #backup database
-mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_NAME | gzip > sql_dump/`data ' %m-%d-%Y'`.sql.gz 
+mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_NAME | gzip > "sql_dump/backup-"$DATE.sql.gz 
 
 #cleanup old files
-find sql_dump -type f -mtime +30 -exec rm '{}' \;
+#find sql_dump -type f -mtime +30 -exec rm '{}' \;
