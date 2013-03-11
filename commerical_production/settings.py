2e40bd6fe802b105df472c4d66f552f42a7b3003
Joshua Blum <joshblum@mit.edu>
2013-03-10 22:38:42 -0400

    dnago admin

--- commerical_production/settings.py
+++ commerical_production/settings.py
@@ -158,6 +158,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django_admin_bootstrapped',
     # Uncomment the next line to enable the admin:
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- scripts/import_db.sh
+++ scripts/import_db.sh
@@ -8,10 +8,6 @@ echo "Starting"
 echo "Exporting data..."
 mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_NAME > dumpfile.sql
 
-echo "Dropping old data"
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e DROP DATABASE $MYSQL_NAME_DEV; 
-mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e CREATE DATABASE $MYSQL_NAME_DEV;
-
 echo "Importing data..."
 mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_NAME_DEV < dumpfile.sql;
 
