2a99b17c8b803967c8ff21d0f9765fbf93993c64
Joshua Blum <joshblum@mit.edu>
2014-08-02 17:36:09 -0400

    user_list

--- commProd/management/commands/update_user_list.py
+++ commProd/management/commands/update_user_list.py
@@ -6,35 +6,37 @@ import os
 
 
 class Command(NoArgsCommand):
-    help = 'Updates autocomplete for the user list.'
+  help = 'Updates autocomplete for the user list.'
 
-    def handle(self, **options):
-        print "Updating user list...\n"
-        profiles = cpm.UserProfile.objects.all().select_related()
-        user_list = []
-        user_dict = {}
-        for profile in profiles.iterator():
-            username = profile.user.username
-            name = profile.user.get_full_name()
-            if not name:
-                name = username
-            user_dict[name] = username
-            user_list.append(name)
-            print "Added", name
+  def handle(self, **options):
+    print "Updating user list...\n"
+    profiles = cpm.UserProfile.objects.all().select_related()
+    user_list = []
+    user_dict = {}
+    for profile in profiles.iterator():
+      username = profile.user.username
+      name = profile.user.get_full_name()
+      if not name:
+        name = username
+      name = str(name)
+      username = str(username)
+      user_dict[name] = username
+      user_list.append(name)
+      print "Added", name
 
-        data = """
+    data = """
             var user_list = %(user_list)s\n
             var user_dict = %(user_dict)s""" % {
-            'user_list': user_list,
-            'user_dict': user_dict,
-        }
+        'user_list': user_list,
+        'user_dict': user_dict,
+    }
 
-        path = os.path.abspath(
-            os.path.join('commerical_production/public/js/',
-                         os.path.pardir))
-        path = "%s/js/user_list.js" % path
+    path = os.path.abspath(
+        os.path.join('commerical_production/public/js/',
+                     os.path.pardir))
+    path = "%s/js/user_list.js" % path
 
-        with open(path, 'w') as f:
-            f.write(data)
+    with open(path, 'w') as f:
+      f.write(data)
 
-        print "Complete.\n"
+    print "Complete.\n"
